d69abdec1cd0396b7013d2ffdb4b4c13022b4fb3
Anuj Sharma <anuj.sharma01@nagarro.com>
2020-02-21 13:35:15 +0530

    Updates

---
+++ precache-manifest.01981c1aa4eb72890e8194f8e98f5fb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec91beaf749a4bf8182e6f19f2e8e131",
+    "url": "/snake-game-reactjs/index.html"
+  },
+  {
+    "revision": "efe3af21a9e498cd57dc",
+    "url": "/snake-game-reactjs/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "174116dc290910e2c9b3",
+    "url": "/snake-game-reactjs/static/css/main.b0cfff49.chunk.css"
+  },
+  {
+    "revision": "efe3af21a9e498cd57dc",
+    "url": "/snake-game-reactjs/static/js/2.77929467.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/snake-game-reactjs/static/js/2.77929467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174116dc290910e2c9b3",
+    "url": "/snake-game-reactjs/static/js/main.26c68a56.chunk.js"
+  },
+  {
+    "revision": "53b3cc6f00c3147190e3",
+    "url": "/snake-game-reactjs/static/js/runtime-main.ec936b88.js"
+  }
+]);
